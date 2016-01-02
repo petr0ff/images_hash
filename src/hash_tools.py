75f0b526da53b95da747d2054c2e2809cc637919
German Petrov <petrff84@gmail.com>
2016-01-02 22:23:57 +0300

    Reworked modules

--- src/hash_tools.py
+++ src/hash_tools.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import csv, os
 import config
-import numpy as np
-import operator
 
 
 def build_dhash(image):
@@ -13,28 +11,6 @@ def build_dhash(image):
     difference = collect_pixel_diff(image)
     # 4. Convert to hexadecimal array
     return convert_to_hex(difference)
-
-
-def weighted_average(pixel):
-    return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
-
-
-def convert_to_grayscale(image):
-    if image.mode == 'RGB':
-        # Getting image as a pixels array
-        pixels = np.asarray(image)
-        # Convert each pixel via formula Y' =  0.299 R' + 0.587 G' + 0.114 B'
-        gray = np.dot(pixels[...,:3], [0.299, 0.587, 0.144])
-        # Overwrite existing image with new pixels array
-        image = Image.fromarray(np.uint8(gray))
-    return image
-
-
-def resize_image(image):
-    return image.resize(
-        (config.hash_size + 1, config.hash_size), # (width, height)
-        Image.BILINEAR, # linear interpolation
-    )
 
 
 def convert_and_resize_image(image):
