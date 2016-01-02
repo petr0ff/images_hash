4a49ed30f8e9f8b8dd1850e352fdb7b315eee302
German Petrov <petrff84@gmail.com>
2016-01-02 18:10:02 +0300

    Added "database" initiation

--- main/find_similar_pics.py
+++ main/find_similar_pics.py
@@ -2,8 +2,6 @@ from PIL import Image
 from src.hash_tools import *
 from src import config
 
-#calculate_hash_strings_in_dir(config.images_dir)
-
 img1 = Image.open(config.test_files_dir + 'test1.jpg')
 scan_for_similarity(img1)
 
---
+++ main/init_database.py
@@ -0,0 +1,4 @@
+from src.hash_tools import *
+from src import config
+
+calculate_hash_strings_in_dir(config.images_dir)
